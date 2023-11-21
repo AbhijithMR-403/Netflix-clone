67f17a6c9fcc76fd594645e95b863eaffac2ff53
Abhijith M R <abhijithmr581@gmail.com>
2023-11-21 17:46:20 +0530

    Arrange image - try 1

--- src/compount/main/Main.jsx
+++ src/compount/main/Main.jsx
@@ -1,45 +1,32 @@
-import React, { useState } from 'react'
 import './main.css'
 import Card from '../card/Card'
-import instance from '../../axios'
-import { API_KEY, img_Base_url } from '../../constants/constants'
-import { useEffect } from 'react'
+import { API_KEY} from '../../constants/constants'
 
 function Main() {
-    const [movie,setMovie] = useState("")
-    useEffect(()=>{
-        instance.get(`movie/now_playing?api_key=${API_KEY}`).then((res)=>{
-          console.log(res.data.results)
-          setMovie(res.data.results[0].backdrop_path)
-        })
-      })
-      console.log(img_Base_url+movie);
     return (
         <div>
             <div className="location" id="home">
                 <h1 id="home">Popular on Netflix</h1>
                 <div className="box">
-                    <Card img_path={img_Base_url+movie} />
+                <Card url={`movie/now_playing?api_key=${API_KEY}`} size={12} />
                 </div>
             </div>
 
-            <h1 id="tvShows">TV Shows</h1>
+      <h1 id="tvShows">TV Shows</h1>
       <div className="box">
-      <Card />             
+      <Card url={`movie/now_playing?api_key=${API_KEY}`} size={6} />
       </div>
       
-
       <h1 id="movies">Blockbuster Action & Adventure</h1>
       <div className="box">
-        
-      <Card />            
+      <Card url={`movie/now_playing?api_key=${API_KEY}`} size={6} />
       </div>
 
       <h1 id="originals">Netflix Originals</h1>
       <div className="box">
-      <Card />             
+      <Card url={`movie/now_playing?api_key=${API_KEY}`} size={6} />
       </div>
-        </div>
+      </div>
     )
 }
 
